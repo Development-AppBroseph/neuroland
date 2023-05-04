f8834a7bafd4eb5d64ea53aeed76bd1ba596c683
Иван Киселев <kkkivannn@MacBook-Pro-uzivatela-Ivan.local>
2023-05-04 22:46:20 +0300

    Notifications

---
+++ ios/noti/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  Noti
+//
+//  Created by Иван Киселев on 04.05.2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
