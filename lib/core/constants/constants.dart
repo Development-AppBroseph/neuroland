5c598ffd02d4bb10c8fca54dee4a9c26c7617356
Иван Киселев <kkkivannn@MacBook-Pro-uzivatela-Ivan.local>
2023-05-04 22:50:54 +0300

    Endpoints

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,43 +1,47 @@
 enum Endpoints {
-  registration,
-  usersMe,
-  authentication,
-  refreshToken,
-  vidoCourses,
-  profile,
-  inviteLink,
-  addPoints,
-  useCoupon,
-  partnerCoupons,
+  registration(endpoint: '/api/registration/'),
+  usersMe(endpoint: '/api/users/me/'),
+  authentication(endpoint: '/api/auth/token/login/'),
+  refreshToken(endpoint: '/api/auth/token/refresh/'),
+  vidoCourses(endpoint: '/api/course-videos/'),
+  profile(endpoint: '/api/users/me/'),
+  inviteLink(endpoint: '/api/users/me/'),
+  addPoints(endpoint: '/api/videos/ viewed/'),
+  useCoupon(endpoint: '/api/coupons/ redeem/'),
+  partnerCoupons(endpoint: '/api/partner-coupons/');
+
+  final String endpoint;
+  const Endpoints({required this.endpoint});
 }
 
 const String baseUrl = 'http://158.160.44.207:8007';
 
-extension EndpointsExtension on Endpoints {
-  String get endpoint {
-    switch (this) {
-      case Endpoints.registration:
-        return '/api/registration/';
-      case Endpoints.usersMe:
-        return '/api/users/me/';
-      case Endpoints.authentication:
-        return '/api/auth/token/login/';
-      case Endpoints.refreshToken:
-        return '/api/auth/token/refresh/';
-      case Endpoints.vidoCourses:
-        return '/api/course-videos/';
-      case Endpoints.profile:
-        return '/api/users/me/';
-      case Endpoints.inviteLink:
-        return '/api/invite/';
-      case Endpoints.addPoints:
-        return '/api/videos/ viewed/';
-      case Endpoints.useCoupon:
-        return '/api/coupons/ redeem/';
-      case Endpoints.partnerCoupons:
-        return '/api/partner-coupons/';
-    }
-  }
-}
+// extension EndpointsExtension on Endpoints {
+//   String get endpoint {
+//     switch (this) {
+//       case Endpoints.registration:
+//         return '/api/registration/';
+//       case Endpoints.usersMe:
+//         return '/api/users/me/';
+//       case Endpoints.authentication:
+//         return '/api/auth/token/login/';
+//       case Endpoints.refreshToken:
+//         return '/api/auth/token/refresh/';
+//       case Endpoints.vidoCourses:
+//         return '/api/course-videos/';
+//       case Endpoints.profile:
+//         return '/api/users/me/';
+//       case Endpoints.inviteLink:
+//         return '/api/invite/';
+//       case Endpoints.addPoints:
+//         return '/api/videos/ viewed/';
+//       case Endpoints.useCoupon:
+//         return '/api/coupons/ redeem/';
+//       case Endpoints.partnerCoupons:
+//         return '/api/partner-coupons/';
+//     }
+//   }
+// }
+
 //TODO переделать свич
 const String telegramBot = 'https://t.me/Neurolandro_bot';
