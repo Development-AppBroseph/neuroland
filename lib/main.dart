b7f1506b014bb2035cefd831c1a646d7577eb410
kkkivannn <ikiselev288@gmail.com>
2023-05-27 17:51:21 +0300

    Add notification icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
-import 'dart:io';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:grow_food/features/presentation/auth/presentation/sign_in/controller/sign_in_cubit.dart';
 import 'package:grow_food/features/presentation/auth/presentation/sign_up/controller/sign_up_cubit.dart';
 import 'package:grow_food/features/presentation/learning_and_coupons/presentation/coupons/controller/partner_coupons_cubit.dart';
-import 'package:grow_food/features/presentation/profile/presentation/controller/profile_cubit.dart';
 import 'package:grow_food/features/presentation/learning_and_coupons/presentation/learning/controller/actual_courses_cubit.dart';
+import 'package:grow_food/features/presentation/profile/presentation/controller/profile_cubit.dart';
 import 'package:grow_food/features/presentation/root_screen/presentation/root_screen/controllers/root_screen_cubit.dart';
 import 'package:grow_food/rout_generatoe.dart';
-import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:onesignal_flutter/onesignal_flutter.dart';
+// import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:path_provider/path_provider.dart';
+
 // import 'package:google_api_availability/google_api_availability.dart';
 import 'injection.container.dart' as di;
 import 'injection.container.dart';
@@ -65,17 +64,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   void initState() {
-    configOnSignal();
+    // configOnSignal();
     super.initState();
   }
 
-  void configOnSignal() async {
-    await OneSignal.shared.setLogLevel(OSLogLevel.debug, OSLogLevel.none);
-    await OneSignal.shared.setAppId('b6c3aa52-66bd-4346-ac64-feadfa8cfcc5');
-    OneSignal.shared.setNotificationWillShowInForegroundHandler((event) {
-      OSNotificationDisplayType.notification;
-    });
-  }
+  // void configOnSignal() async {
+  //   await OneSignal.shared.setLogLevel(OSLogLevel.debug, OSLogLevel.none);
+  //   await OneSignal.shared.setAppId('b6c3aa52-66bd-4346-ac64-feadfa8cfcc5');
+  //   OneSignal.shared.setNotificationWillShowInForegroundHandler((event) {
+  //     OSNotificationDisplayType.notification;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
